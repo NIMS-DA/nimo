6c3eb923a850d564f407144f1e63d2685b286a1b
Ryo Tamura <37391015+rtmr@users.noreply.github.com>
2024-08-18 16:07:33 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nimo",
+    version="1.0.0",
+    author="NIMO developers",
+    license="MIT",
+    description='NIMO package',
+    packages=["nimo", "nimo.ai_tools", "nimo.input_tools", "nimo.output_tools", "nimo.visualization"],
+    install_requires=[
+        "Cython",
+        "matplotlib",
+        "numpy",
+        "physbo>=2.0.0",
+        "scikit-learn",
+        "scipy"
+    ]
+)
