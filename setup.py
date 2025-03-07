79759149dd390341c44b2918eeeacf89b5cd0f3d
Ryo Tamura <37391015+rtmr@users.noreply.github.com>
2025-03-07 16:10:39 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nimo",
-    version="1.0.0",
+    version="1.0.5",
     author="NIMO developers",
     license="MIT",
     description='NIMO package',
